55b0f1c225a7b7348bad0233d5f9bc32f183d9cd
Hilll_ <91865375+Hilll19@users.noreply.github.com>
2022-11-08 22:13:23 +0700

    Add files via upload

---
+++ Week10SSIP/database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Film (
+    Name_Film varchar(50) PRIMARY KEY not NULL,
+    Genre_Film varchar(50) not NULL,
+    Duration_Film varchar(50) not NULL,
+    Release_Film int(4) not NULL
+);
+INSERT INTO Film (Name_Film, Genre_Film, Duration_Film, Release_Film)VALUES;
