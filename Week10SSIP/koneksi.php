e645abfc71fd5ba78130a71ed0ce62a7b741d9ed
Hilll_ <91865375+Hilll19@users.noreply.github.com>
2022-11-08 22:18:44 +0700

    Delete Week10SSIP directory

--- Week10SSIP/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = new mysqli("localhost", "root", "", "film");
-    if ($conn->connect_error) {
-        echo "connection failed : ".$conn->connect_error;
-    }else{
-        echo "Success connection";
-    }
-    $conn->close();
-?>
